5e597f6dd9fee95c215d988c08d1dddcaeb5c748
joulecpa <joulecpa@gmail.com>
2015-03-03 17:56:50 -0500

    Starting partB

---
+++ A2/PartB/Picture.java
@@ -0,0 +1,12 @@
+/**
+ * The Picture class holds instances of the Shape class.
+ * <p>
+ * It will implment two methods getPerimeter and getArea which will get the
+ * the total perimeter and area of all the Shapes in the Picture.
+ * @author Hong Zhan Huang hzhuang3@myseneca.ca
+ * @version 1.0
+ */
+public class Picture{
+
+  Shape[] shapes;
+}
